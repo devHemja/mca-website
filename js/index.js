bc5f6e6b46df6e7f57b359901be86544ff4c0740
Amrit Utsav <lbrock513@gmail.com>
2024-10-30 03:35:22 +0530

    responsive 2nd - finaltouch

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 function myFunction() {
+    var icon = document.getElementById("collapseIcon");
     var x = document.getElementById("collapse");
     if (x.className === "nav_items") {
         x.className += " responsive_nav";
+        icon.className = "bi bi-x-circle";
+        icon.style.fontSize = '1.7rem';
     } else {
         x.className = "nav_items";
+        icon.className = "bi bi-list";
+        icon.style.fontSize = '2rem';
     }
 }
